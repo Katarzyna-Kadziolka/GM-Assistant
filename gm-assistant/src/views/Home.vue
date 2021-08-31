f964feae6646ee5ce122b77be015e5ae697a22b1
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-08-31 19:52:02 +0200

    Start layout

--- gm-assistant/src/views/Home.vue
+++ gm-assistant/src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
-  <hello-world />
+  <v-app>
+    <v-app-bar app>
+      <div class="pl-6">
+        <h2>GM Assistant</h2>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+      </v-container>
+    </v-main>
+</v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '../components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'Home',
 
-  components: {
-    HelloWorld
-  }
+  components: {}
 })
 </script>
