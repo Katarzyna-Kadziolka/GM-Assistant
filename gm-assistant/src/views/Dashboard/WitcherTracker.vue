8538b44eecb51499e073a0e2f21cd9cf8b0c3458
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-10-10 22:57:16 +0200

    Add table

--- gm-assistant/src/views/Dashboard/WitcherTracker.vue
+++ gm-assistant/src/views/Dashboard/WitcherTracker.vue
@@ -1,24 +1,82 @@
 <template>
-  <v-app>
-    <v-container class="grey lighten-5">
-      <v-row no-gutters>
-        <v-col
-          v-for="n in 3"
-          :key="n"
-          cols="12"
-          sm="4"
-        >
-          <v-card
-            class="pa-2"
-            outlined
-            tile
-          >
-            One of three columns
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app>
+<div class="table-responsive">
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">Initiative</th>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Dodge</th>
+      <th scope="col">Fist fight</th>
+      <th scope="col">Intellect</th>
+      <th scope="col" data-toggle="tooltip" data-placement="top" title="Magic resistance">Magic res.</th>
+      <th scope="col">Armor</th>
+      <th scope="col">HP</th>
+      <th scope="col"></th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody v-for="(row, key) in startAmmountOfRowns" :key="{ key }">
+    <tr>
+      <td>
+        <div class="input-group">
+          <input type="text" class="form-control">
+        </div>
+      </td>
+      <td>
+        <div class="input-group">
+          <input type="text" class="form-control">
+        </div>
+      </td>
+      <td>
+        <div class="input-group">
+          <input type="text" class="form-control">
+        </div>
+      </td>
+      <td>
+        <div class="input-group">
+          <input type="text" class="form-control">
+        </div>
+      </td>
+      <td>
+        <div class="input-group">
+          <input type="text" class="form-control">
+        </div>
+      </td>
+      <td>
+        <div class="input-group">
+          <input type="text" class="form-control">
+        </div>
+      </td>
+      <td>
+        <div class="input-group">
+          <input type="text" class="form-control">
+        </div>
+      </td>
+      <td>
+        <div class="input-group">
+          <input type="text" class="form-control">
+        </div>
+      </td>
+      <td>
+        <div class="input-group">
+          <input type="text" class="form-control">
+        </div>
+      </td>
+      <td>
+        <div class="input-group">
+          <button type="button" class="btn btn-light btn_width"><b>+5</b></button>
+        </div>
+      </td>
+      <td>
+        <div class="input-group">
+          <button type="button" class="btn btn-light btn_width"><b>-5</b></button>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
+</div>
 </template>
 
 <script lang="ts">
@@ -29,21 +87,14 @@ export default defineComponent({
   name: 'WitcherTracker',
   data: () => {
     return {
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237
-        },
-        {
-          name: 'Eclair',
-          calories: 262
-        }
-      ]
+      startAmmountOfRowns: 5
     }
   }
 })
 </script>
+
+<style lang="scss" scoped>
+  .btn_width {
+    width: 40px
+  }
+</style>
